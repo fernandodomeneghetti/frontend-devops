bfb9e6b330541542b0985a2b86352de4e9401740
Fernando Miranda Domeneghetti <158805760+fernandodomeneghetti@users.noreply.github.com>
2025-08-05 17:57:28 -0300

    Update site-facul.cy.js

--- integration-test/cypress/e2e/site-facul.cy.js
+++ integration-test/cypress/e2e/site-facul.cy.js
@@ -1,11 +1,11 @@
 describe('Página Inicial', () => {
   it('Deve carregar corretamente a página', () => {
-    cy.visit('http://localhost:5500/site-aula.html')
+    cy.visit('http://localhost:5500/index.html')
     cy.contains('Faculdade Tech')
   });
 
   it('Deve navegar até a página de Cursos', () => {
-    cy.visit('http://localhost:5500/site-aula.html');
+    cy.visit('http://localhost:5500/index.html');
     cy.get('nav a').contains('Cursos').click();
     cy.url().should('include', 'cursos.html');
   });
